6094e36acd07405018917f9b4eb16015787f7077
Paul Tiller <pftiller@gmail.com>
2018-06-26 09:52:47 -0500

    pointing to html

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
-const express = require('express')
-const app = express()
-app.get('/', (req, res) => {
-  res.send('HEY!')
-})
-app.listen(3000, () => console.log('Server running on port 3000'))
+const express = require('express');
+const app = express();
+app.use(express.static('public'));
+app.listen(3000, () => console.log('Server running on port 3000'));
