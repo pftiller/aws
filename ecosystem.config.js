6163d73b7550c2c7bbaf2fa91787202a9e1a95ad
Paul Tiller <pftiller@gmail.com>
2018-06-26 02:54:30 -0500

    connecting SSH

---
+++ ecosystem.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+    apps: [{
+      name: 'aws',
+      script: 'server.js'
+    }],
+    deploy: {
+      production: {
+        user: 'ubuntu',
+        host: 'ec2-18-191-226-191.us-east-2.compute.amazonaws.com',
+        key: '~/.ssh/portfolio.pem',
+        ref: 'origin/master',
+        repo: 'git@github.com:pftiller/aws.git',
+        path: '/home/ubuntu/aws',
+        'post-deploy': 'npm install && pm2 startOrRestart ecosystem.config.js'
+      }
+    }
+  }
