b58ad94284a184814d4a4c5875cf6018a5157f84
Paul Tiller <pftiller@gmail.com>
2018-06-26 07:59:36 -0500

    pointing to index.html

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
 const express = require('express')
 const app = express()
 app.use(express.static('public'));
-app.listen(2030, () => console.log('Server running on port 2030'))
+app.listen(3000, () => console.log('Server running on port 3000'))
